adf2079ecc470e2e3c0eecf3475326df3913d7b1
Christopher Li <Christopherli2017@u.northwestern.edu>
2017-07-24 11:25:53 +0900

    Sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :users
   root 'static_pages#home'
