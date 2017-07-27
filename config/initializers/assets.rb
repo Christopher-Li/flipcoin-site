234e75562e2ac45c8679c331c2489fc453ce6452
Christopher Li <Christopherli2017@u.northwestern.edu>
2017-07-27 13:35:22 +0800

    update homepage and simple design for login page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( particles.js )
 Rails.application.config.assets.precompile += %w( particles-app.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( jquery-3.2.1.min.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
