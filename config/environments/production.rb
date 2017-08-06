6b5c196f71643cf870f0b41df3a03da5a53fb979
Christopher Li <Christopherli2017@u.northwestern.edu>
2017-08-07 01:50:57 +0800

    change url to flipcoinsale.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'salty-dawn-66177.herokuapp.com'
+  host = 'flipcoinsale.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
