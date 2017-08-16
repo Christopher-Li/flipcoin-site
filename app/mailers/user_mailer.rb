2b24794cc0e7d9be4920714cd98007d677bd0727
Jack Kasbeer <jkasbeer2@gmail.com>
2017-08-16 18:37:53 -0500

    fix flipcoin spelling in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Activate your Flipcoin account today!"
+    mail to: user.email, subject: "Activate your FlipCoin account today!"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
