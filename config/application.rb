1053dce994068f1fbc49e8bb943748b8579ab1f7
Jack Kasbeer <jkasbeer2@gmail.com>
2017-08-19 20:33:23 -0500

    hoping to finally rid the site of alan'

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module FlipcoinSite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # Fix build error & heroku fail to push
+	config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
