fd5d20cef0ecfa443203395436da3112f5ca86a6
Christopher Li <Christopherli2017@u.northwestern.edu>
2017-08-30 19:47:18 -0700

    bring back rolling links in general js

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,24 +1,24 @@
 /* ROLLING LINKS
  ******************/
 
-// var supports3DTransforms =  document.body.style['webkitPerspective'] !== undefined || 
-//                               document.body.style['MozPerspective'] !== undefined;
-// function linkify( selector ) {
-//     if( supports3DTransforms ) {
+var supports3DTransforms =  document.body.style['webkitPerspective'] !== undefined || 
+                              document.body.style['MozPerspective'] !== undefined;
+function linkify( selector ) {
+    if( supports3DTransforms ) {
         
-//         var nodes = document.querySelectorAll( selector );
+        var nodes = document.querySelectorAll( selector );
 
-//         for( var i = 0, len = nodes.length; i < len; i++ ) {
-//             var node = nodes[i];
+        for( var i = 0, len = nodes.length; i < len; i++ ) {
+            var node = nodes[i];
 
-//             if( !node.className || !node.className.match( /roll/g ) ) {
-//                 node.className += ' roll';
-//                 node.innerHTML = '<span data-title="'+ node.text +'">' + node.innerHTML + '</span>';
-//             }
-//         };
-//     }
-// } 
-// linkify( 'a.linki' );
+            if( !node.className || !node.className.match( /roll/g ) ) {
+                node.className += ' roll';
+                node.innerHTML = '<span data-title="'+ node.text +'">' + node.innerHTML + '</span>';
+            }
+        };
+    }
+} 
+linkify( 'a.linki' );
 
 /* SMOOTH SCROLLING
  ********************/
