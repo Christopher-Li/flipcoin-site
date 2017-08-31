1b644af2149e037e4e4d4933b52750f38d630ee0
Christopher Li <Christopherli2017@u.northwestern.edu>
2017-08-30 20:29:48 -0700

    restore sidenav and switch to jquery 2

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -63,13 +63,13 @@ $('a[href*="#"]')
 
 // Materialize css
 // mobile navbar
-// $('.button-collapse').sideNav({
-//     menuWidth: 300, // Default
-//     edge: 'left', // Choose the horizontal origin
-//     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-//     draggable: true // Choose whether you can drag to open on touch screens,
-//   }
-// );
+$('.button-collapse').sideNav({
+    menuWidth: 300, // Default
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens,
+  }
+);
 
 // date picker for registration
 $('.datepicker').pickadate({
